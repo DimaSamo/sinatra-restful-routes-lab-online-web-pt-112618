14de9676c466231af571368cb9c17c615c8e2a20
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-02 04:59:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/recipes/:id' do
+    @recipe.find(params[:id])
     Recipe.update(params[:id], name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     # @recipe=Recipe.find(params[:id])
     # @recipe.name=params[:name]
